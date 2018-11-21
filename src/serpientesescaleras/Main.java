2067351630e16764c3bc9c9f44e7f1717a41d297
Carlos Contreras <cdca12an@gmail.com>
2018-11-21 16:11:17 -0700

    Refactorizado Main
    Simulación del juego hecho en un método

--- src/serpientesescaleras/Main.java
+++ src/serpientesescaleras/Main.java
@@ -26,93 +26,21 @@ public class Main {
             generarCasilla(tablero, 'S', 30, 95);
         }
 
-        // TEST: Comprobar tablero
-        System.out.println("\t\t\t\t TABLERO"
+        // Imprimir tablero
+        System.out.println("\t\t\t\t  TABLERO"
                 + "\n---------------------------------------------------------------------------------");
         imprimirTablero(tablero);
-        
+        System.out.println("---------------------------------------------------------------------------------");
 
         // Generar jugadores
         NodoDBL<Casilla>[] jugadores = new NodoDBL[new Random().nextInt(10 - 2 + 1) + 2];
 
-        
         // Simulación juego
         System.out.println("\n\n\tSIMULACIÓN DE JUEGO"
                 + "\nNúmero de jugadores: " + jugadores.length);
-        
-        int dados, turno = 0, i = 0;
-        boolean juegoCompletado = false;
-        NodoDBL<Casilla> aux;
-        while (!juegoCompletado) {
-            for (i = 0; i < jugadores.length; i++) {
-                turno++;
-                dados = new Random().nextInt(12 - 2 + 1) + 2;
-
-                System.out.println("----------------------------------"
-                        + "\nTurno " + turno + ""
-                        + "\nJugador: " + (i + 1)
-                        + "\nDados: " + dados);
-
-                // Primer tiro de ese jugador
-                if (jugadores[i] == null) {
-                    jugadores[i] = tablero.getFrente();
-                    for (int j = 0; j < dados - 1; jugadores[i] = jugadores[i].getSig(), j++);
-                    System.out.println(
-                            "Casilla inicio: Primer tiro"
-                            + "\nCasilla final: " + jugadores[i].Info.noCasilla
-                            + "\nTipo de nodo: N"
-                            + "\n----------------------------------\n");
-                    continue;
-                }
-
-                System.out.println("Casilla inicio: " + jugadores[i].Info.noCasilla);
-
-                // Me posiciono donde va a llegar el jugador
-                for (int j = 0; j < dados; j++) {
-                    if (jugadores[i].getSig() == null) { // Se llega al final del tablero
-                        for (int k = 0; k < dados - j; jugadores[i] = jugadores[i].getAnt(), k++); // Se regresa
-                        break;
-                    }
-                    jugadores[i] = jugadores[i].getSig();
-                }
-
-                aux = jugadores[i]; // Guardar donde cayó inicialmente
-
-                // Dependiento en qué tipo de casilla cayó, moverlo.
-                if (jugadores[i].Info.tipoCasilla == 'N') {
-                    System.out.println("Casilla final: " + jugadores[i].Info.noCasilla
-                            + "\nTipo de nodo: N"
-                            + "\n----------------------------------\n");
-                } else if (jugadores[i].Info.tipoCasilla == 'E') {
-                    for (int j = 0; j < jugadores[i].Info.posiciones; aux = aux.getSig(), j++);
-                    System.out.println("Casilla final: " + aux.Info.noCasilla
-                            + "\nTipo de nodo: E"
-                            + "\nEscaleras - Avanzaste " + jugadores[i].Info.posiciones + " posiciones"
-                            + "\n----------------------------------\n");
-                    jugadores[i] = aux;
-                } else if (jugadores[i].Info.tipoCasilla == 'S') {
-                    for (int j = 0; j < jugadores[i].Info.posiciones; aux = aux.getAnt(), j++);
-                    System.out.println("Casilla final: " + aux.Info.noCasilla
-                            + "\nTipo de nodo: S"
-                            + "\nSerpientes - Retrocediste " + jugadores[i].Info.posiciones + " posiciones"
-                            + "\n----------------------------------\n");
-                    jugadores[i] = aux;
-                }
-
-                // Ganar el juego
-                if (jugadores[i].Info.noCasilla == 100) {
-                    System.out.println("----------------------------------"
-                            + "\n\t¡JUEGO TERMINADO!"
-                            + "\n     El ganador es: Jugador " + (i + 1)
-                            + "\n----------------------------------");
-                    juegoCompletado = true;
-                    break;
-                }
-            }
-        }
+        simularJuego(tablero, jugadores);
     }
 
-    // Método imprimir tablero para tests
     public static void imprimirTablero(ListaDBL<Casilla> T) {
         NodoDBL<Casilla> aux = T.getFrente();
         for (int i = 0; aux != null && i < 10; i++) {
@@ -157,4 +85,75 @@ public class Main {
         auxTermina.Info.tipoCasilla = 'T';
     }
 
+    public static void simularJuego(ListaDBL<Casilla> tablero, NodoDBL<Casilla>[] jugadores) {
+        int dados, turno = 0;
+        boolean juegoCompletado = false;
+        NodoDBL<Casilla> aux;
+        while (!juegoCompletado) {
+            for (int i = 0; i < jugadores.length; i++) {
+                turno++;
+                dados = new Random().nextInt(12 - 2 + 1) + 2;
+
+                System.out.println("----------------------------------"
+                        + "\nTurno " + turno + ""
+                        + "\nJugador: " + (i + 1)
+                        + "\nDados: " + dados);
+
+                // Primer tiro de ese jugador
+                if (jugadores[i] == null) {
+                    jugadores[i] = tablero.getFrente();
+                    for (int j = 0; j < dados - 1; jugadores[i] = jugadores[i].getSig(), j++);
+                    System.out.println(
+                            "Casilla inicio: Primera jugada"
+                            + "\nCasilla final: " + jugadores[i].Info.noCasilla
+                            + "\nTipo de nodo: N"
+                            + "\n----------------------------------\n");
+                    continue;
+                }
+
+                System.out.println("Casilla inicio: " + jugadores[i].Info.noCasilla);
+
+                // Me posiciono donde va a llegar el jugador
+                for (int j = 0; j < dados; j++) {
+                    if (jugadores[i].getSig() == null) { // Se llega al final del tablero
+                        for (int k = 0; k < dados - j; jugadores[i] = jugadores[i].getAnt(), k++); // Se regresa
+                        break;
+                    }
+                    jugadores[i] = jugadores[i].getSig();
+                }
+
+                aux = jugadores[i]; // Guardar donde cayó inicialmente
+
+                // Dependiento en qué tipo de casilla cayó, moverlo.
+                if (jugadores[i].Info.tipoCasilla == 'N') {
+                    System.out.println("Casilla final: " + jugadores[i].Info.noCasilla
+                            + "\nTipo de nodo: N");
+                } else if (jugadores[i].Info.tipoCasilla == 'E') {
+                    for (int j = 0; j < jugadores[i].Info.posiciones; aux = aux.getSig(), j++);
+                    System.out.println("Casilla final: " + aux.Info.noCasilla
+                            + "\nTipo de nodo: E"
+                            + "\nEscaleras - Avanzaste " + jugadores[i].Info.posiciones + " posiciones");
+                    jugadores[i] = aux;
+                } else if (jugadores[i].Info.tipoCasilla == 'S') {
+                    for (int j = 0; j < jugadores[i].Info.posiciones; aux = aux.getAnt(), j++);
+                    System.out.println("Casilla final: " + aux.Info.noCasilla
+                            + "\nTipo de nodo: S"
+                            + "\nSerpientes - Retrocediste " + jugadores[i].Info.posiciones + " posiciones");
+                    jugadores[i] = aux;
+                }
+                System.out.println("----------------------------------\n");
+
+                // Ganar el juego
+                if (jugadores[i].Info.noCasilla == 100) {
+                    System.out.println("----------------------------------"
+                            + "\n\t¡JUEGO TERMINADO!"
+                            + "\n     El ganador es: Jugador " + (i + 1)
+                            + "\n----------------------------------");
+                    juegoCompletado = true;
+                    break;
+                }
+            }
+        }
+    }
+    
 }
